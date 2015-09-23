147cbf0e5d1648946c7303c005de7a1b644a5101
Mykees <mykeespro@gmail.com>
2015-09-23 15:51:35 +0200

    add documentation

--- Controller/TagsController.php
+++ Controller/TagsController.php
@@ -10,6 +10,12 @@ class TagsController extends Controller
     public function manage(){
         return $this->getDoctrine()->getManager();
     }
+
+	/**
+	 * Unlink and delete a relation
+	 * @param $relation_id
+	 * @return Response
+	 */
     public function deleteRelationAction( $relation_id ){
         $tagRelation = $this->manage()->getRepository('MykeesTagBundle:TagRelation')->find($relation_id);
         $this->manage()->remove($tagRelation);
